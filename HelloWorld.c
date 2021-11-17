ed17589cef6466e51e9c275997653b50d86649a3
Leonardo Duarte <61410466+leo01eidt@users.noreply.github.com>
2021-11-17 12:03:33 -0300

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,21 @@
+/* Esse programa tem por objetivo imprimir um "Hello World" em C. */
+
+#include<stdio.h>
+
+int main()
+{
+    printf("\nHello World.\n\n"); /* Toda a linha, incluindo o prinft() é chamado de uma instrução */
+    return 0;                     /* Todas as instruções devem terminar com (;) (ponto e virgula). Isso se informa o fim da instrução.*/
+}
+
+/* A linha main() é uma parte de todo programa em C.
+Os parantêses após o main() indicam um bloco de construção do programa. Chamado de função.
+Todos os programas em C começam a ser executados pela função main().
+
+É uma boa prática precender sua função com um comentário explicando seu objetivo.
+
+\n se chama sequência de escape. o \n faz com que o cursor seja movido ao final do programa para a esquerda.
+Existe diversos switchs para sequência de escape.
+\n Faz com que seja gerado uma nova linha. */
+
+
---
+++ soma.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+            int inteiro1, inteiro2, soma;
+            printf("Digite seu número.\n");
+            scanf("%d", &inteiro1);
+            printf("Digite seu segundo número.\n");
+            scanf("%d", &inteiro2);
+            soma = inteiro1 + inteiro2;
+            printf("A soma dos números é %d/n", soma);
+            return 0;
+}
+
